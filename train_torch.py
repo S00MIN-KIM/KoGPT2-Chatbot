bf6b9c9a1d5fade9d26bade87e8002085e1a0167
S00MIN-KIM <68471619+S00MIN-KIM@users.noreply.github.com>
2021-12-03 16:14:12 +0900

    Update train_torch.py

--- train_torch.py
+++ train_torch.py
@@ -184,7 +184,7 @@ class KoGPT2Chat(LightningModule):
         return torch.LongTensor(data), torch.LongTensor(mask), torch.LongTensor(label)
 
     def train_dataloader(self):
-        data = pd.read_csv('Chatbot_data/ChatbotData.csv')
+        data = pd.read_csv('Chatbot_data/ChatbotData.csv', encoding='cp949')
         self.train_set = CharDataset(data, max_len=self.hparams.max_len)
         train_dataloader = DataLoader(
             self.train_set, batch_size=self.hparams.batch_size, num_workers=2,
