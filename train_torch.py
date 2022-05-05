8c8a13ad020259581d66b89f858915601a8fca75
S00MIN-KIM <68471619+S00MIN-KIM@users.noreply.github.com>
2022-05-05 19:09:32 +0900

    update module

--- train_torch.py
+++ train_torch.py
@@ -11,6 +11,7 @@ from pytorch_lightning.core.lightning import LightningModule
 from torch.utils.data import DataLoader, Dataset
 from transformers.optimization import AdamW, get_cosine_schedule_with_warmup
 from transformers import PreTrainedTokenizerFast, GPT2LMHeadModel
+import torchtext.legacy as torchtext
 
 parser = argparse.ArgumentParser(description='Simsimi based on KoGPT-2')
 
